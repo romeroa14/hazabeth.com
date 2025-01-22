35ed2825dd49b9dbf7767a8e02d5edd478554994
Alfredo Romero <Alfredo Romero>
2025-01-22 11:56:23 -0400

    barra anuncios19

--- assets/scroll-behavior.js
+++ assets/scroll-behavior.js
@@ -1,25 +1,26 @@
 let lastScrollTop = 0;
 const header = document.querySelector('.header-wrapper');
-const scrollThreshold = 100;
+const scrollThreshold = 50;
 
 window.addEventListener('scroll', () => {
-  const currentScroll = window.scrollY || document.documentElement.scrollTop;
+  const currentScroll = window.pageYOffset || document.documentElement.scrollTop;
   
   // Añadir efecto de transparencia
-  if (currentScroll > 0) {
-    console.log('currentScroll', currentScroll);
+  if (currentScroll > 20) {
     header.classList.add('scrolled');
   } else {
     header.classList.remove('scrolled');
   }
   
-  // Ocultar/mostrar header
+  // Ocultar/mostrar header con un pequeño delay
   if (currentScroll > lastScrollTop && currentScroll > scrollThreshold) {
-    // Scrolling hacia abajo
-    header.classList.add('hide-up');
+    requestAnimationFrame(() => {
+      header.classList.add('hide-up');
+    });
   } else {
-    // Scrolling hacia arriba
-    header.classList.remove('hide-up');
+    requestAnimationFrame(() => {
+      header.classList.remove('hide-up');
+    });
   }
   
   lastScrollTop = currentScroll <= 0 ? 0 : currentScroll;
